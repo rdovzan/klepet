7ca1f351f0351ca50a6a9b846d0c59e893d6da0d
Robert Dovžan <rdovzan@gmail.com>
2016-04-10 16:47:56 +0000

    Zaključek dela na domači nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -4,7 +4,7 @@ function divElementEnostavniTekst(sporocilo) {
   var jeVideo = sporocilo.search(/https:\/\/www\.youtube\.com\/watch\?v=([^\s]+)/g) > -1;
   if (jeSmesko || jeSlika || jeVideo) {
     sporocilo = sporocilo.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace('&lt;img', '<img').replace('png\' /&gt;', 'png\' />');
-    sporocilo = sporocilo.replace(/https?:\/\/(\S+?)\.(jpg|png|gif)[^\s]+/g, "<a href='$&'><img src='$&' /><a/>");
+    sporocilo = sporocilo.replace(/https?:\/\/(\S+?)\.(jpg|png|gif)/g, "<a href='$&'><img src='$&' /><a/>");
     sporocilo = sporocilo.replace(/https:\/\/www\.youtube\.com\/watch\?v=([^\s]+)/g, "<iframe src='https://www.youtube.com/embed/$1' allowfullscreen></iframe>");
     return $('<div style="font-weight: bold"></div>').html(sporocilo);
   } else {
